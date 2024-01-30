759318759cfc5919190ec3250795470d0a28d41b
MargaridaEstrela <margaridaestrela@tecnico.ulisboa.pt>
2024-01-30 19:09:12 +0000

    added animation to playing item
    clicking on the item, now can hide the label

--- sketchybar/items/playing.sh
+++ sketchybar/items/playing.sh
@@ -1,6 +1,6 @@
 sketchybar --add item playing left                                                 \
            --set playing update_freq=5                                             \
-                       padding_left=60                                            \
+                       padding_left=60                                             \
                        icon.color="$YELLOW"                                        \
                        label.font="$FONT:Bold:12.0"                                \
                        label.color="$YELLOW"                                       \
@@ -8,3 +8,4 @@ sketchybar --add item playing left                                              
                        label.y_offset=-1                                           \
                        background.padding_right=3                                  \
                        icon.font="$FONT:Regular:14.8"                              \
+           --subscribe playing playing_change mouse.clicked
--- sketchybar/plugins/playing.sh
+++ sketchybar/plugins/playing.sh
@@ -5,3 +5,19 @@ PLAYING=$(cd ~/.config/sketchybar/spotify && ./song.sh)
 FINAL=$(echo $PLAYING | sed 's/\(.\{20\}\).*/\1.../')
 
 sketchybar --set $NAME icon="􁁒 " label=" $FINAL"
+
+click() {
+  CURRENT_WIDTH="$(sketchybar --query $NAME | jq -r .label.width)"
+
+  WIDTH=1
+  if [ "$CURRENT_WIDTH" -eq "1" ]; then
+    WIDTH=dynamic
+  fi
+
+  sketchybar --animate sin 20 --set $NAME label.width="$WIDTH"
+}
+
+case "$SENDER" in
+  "mouse.clicked") click
+  ;;
+esac
