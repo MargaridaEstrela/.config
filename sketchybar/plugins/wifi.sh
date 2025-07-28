97edb3c05d7ce76ab539dd74d0d87e4ba85fa80c
MargaridaEstrela <margaridaestrela@tecnico.ulisboa.pt>
2025-07-28 11:58:37 +0100

    update wifi

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -1,29 +1,65 @@
 #!/bin/bash
 
 update() {
+  echo "DEBUG: ICON=$ICON | SSID=$SSID | WIFI_IP=$WIFI_IP | LABEL=$LABEL" > /tmp/sketchybar_wifi_debug.log
+
   source "$CONFIG_DIR/icons.sh"
-  INFO="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F ' SSID: '  '/ SSID: / {print $2}')"
-  LABEL="$INFO ($(ipconfig getifaddr en0))"
-  ICON="$([ -n "$INFO" ] && echo "$WIFI_CONNECTED" || echo "$WIFI_DISCONNECTED")"
-  COLOR="$([ -n "$INFO" ] && echo "$GREEN" || echo "$RED")"
 
-  sketchybar --set $NAME icon="$ICON" color=$COLOR label="$LABEL"
-}
+  WIFI_IF=$(networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}')
+  SSID=$(ipconfig getsummary "$WIFI_IF" 2>/dev/null | awk -F ' SSID : ' '/ SSID : / {print $2}')
+  WIFI_IP=$(ipconfig getifaddr "$WIFI_IF" 2>/dev/null)
 
-click() {
-  CURRENT_WIDTH="$(sketchybar --query $NAME | jq -r .label.width)"
+  if [[ -n "$SSID" && -n "$WIFI_IP" ]]; then
+    ICON="$WIFI_CONNECTED"
+    COLOR="$GREEN"
+    LABEL="$SSID ($WIFI_IP)"
+  else
+    # Fallback: first active interface
+    ACTIVE_IF=$(ifconfig -l | tr ' ' '\n' | while read -r iface; do
+      IP=$(ipconfig getifaddr "$iface" 2>/dev/null)
+      if [ -n "$IP" ]; then
+        echo "$iface"
+        break
+      fi
+    done)
 
-  WIDTH=0
-  if [ "$CURRENT_WIDTH" -eq "0" ]; then
-    WIDTH=dynamic
+    if [ -n "$ACTIVE_IF" ]; then
+      IP=$(ipconfig getifaddr "$ACTIVE_IF")
+      if [[ "$ACTIVE_IF" == en* ]]; then
+        ICON="$WIFI_ETHERNET"
+        COLOR="$BLUE"
+        LABEL="Ethernet ($IP)"
+      else
+        ICON="$WIFI_CONNECTED"
+        COLOR="$GREEN"
+        LABEL="$ACTIVE_IF ($IP)"
+      fi
+    else
+      ICON="$WIFI_DISCONNECTED"
+      COLOR="$RED"
+      LABEL="No Network"
+    fi
   fi
 
-  sketchybar --animate sin 20 --set $NAME label.width="$WIDTH"
+
+  echo "DEBUG: ICON=$ICON | SSID=$SSID | WIFI_IP=$WIFI_IP | LABEL=$LABEL" > /tmp/sketchybar_wifi_debug.log
+
+
+  sketchybar --set $NAME icon="$ICON" color=$COLOR label="$LABEL"
+  
+}
+
+
+click() {
+  CURRENT_WIDTH="$(sketchybar --query "$NAME" | jq -r .label.width)"
+  WIDTH=0
+  if [ "$CURRENT_WIDTH" -eq 0 ]; then
+    WIDTH=dynamic
+  fi
+  sketchybar --animate sin 20 --set "$NAME" label.width="$WIDTH"
 }
 
 case "$SENDER" in
-  "wifi_change") update
-  ;;
-  "mouse.clicked") click
-  ;;
+  "wifi_change") update ;;
+  "mouse.clicked") click ;;
 esac
